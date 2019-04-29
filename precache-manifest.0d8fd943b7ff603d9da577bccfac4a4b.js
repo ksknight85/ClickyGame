c317dcc3b8fdfd0410a50ae51fe30c0e19b4b666
ksknight <korvette_00Whotmail.com>
2019-04-28 22:09:41 -0600

    Updates

---
+++ precache-manifest.0d8fd943b7ff603d9da577bccfac4a4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd9f52df29c1c3742cfa51c81fdc924",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "1f1170d45c4e644e7a76",
+    "url": "/ClickyGame/static/css/main.02709249.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1f1170d45c4e644e7a76",
+    "url": "/ClickyGame/static/js/main.9de09492.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
